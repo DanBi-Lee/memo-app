85887241c035828800b9432de462812f35048205
DanBi-Lee <danbi.s.rain@gmail.com>
2021-08-16 14:14:11 +0900

    [#4] 메모 아이템 추가 기능 Resolves #4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
 import Modal from './modal.js';
 import TabMenu from './tabmenu.js';
+import Memo from './memo.js';
 
 const modal = new Modal('#buttonAdd', '.modal', '.modal .close');
 const tabMemu  = new TabMenu('.modal-dialog');
+const memo = new Memo('.memos');
+
+const formBox = document.querySelector('.box-form');
+formBox?.addEventListener('click', e => {
+    e.preventDefault();
+    const target = e.target as HTMLElement;
+    if(target.nodeName !== "BUTTON"){
+        return;
+    }
+    const container =  target.closest('.form-box') as HTMLInputElement;
+    const content =  container?.querySelector('[name="memoContent"]') as HTMLInputElement;
+    const title = container?.querySelector('[name="memoTitle"]') as HTMLInputElement;
+    const type = container?.dataset.type as 'image' | 'video' | 'text' | 'task';
+    const memoData = {
+        type,
+        data : {
+            content : content.value,
+            title : title.value
+        }
+    };
+    
+    memo.addMemo(memoData);
+});
---
+++ src/memo.ts
@@ -0,0 +1,97 @@
+type MemoData = {
+    title: string;
+    content: string;
+}
+
+type MemoType = {
+    data : MemoData;
+    type : 'image' | 'video' | 'text' | 'task';
+};
+
+interface MemoInterface {
+    memos : MemoType[];
+    addMemo : (memo:MemoType) => void;
+}
+
+export default class Memo implements MemoInterface {
+    memoBox : HTMLElement|null;
+    memos : MemoType[];
+
+    constructor(memoBoxSelector:string){
+        this.memos = [];
+        this.memoBox = document.querySelector(memoBoxSelector);
+    }
+
+    addMemo(memo:MemoType){
+        let template;
+        const {type} = memo;
+
+        switch(type){
+            case 'image' : 
+                template = this.addImage(memo);
+                break;
+            case 'video':
+                template = this.addVideo(memo);
+                break;
+            case 'text' :
+                template = this.addText(memo);
+                break;
+            case 'task' :
+                template = this.addTask(memo);
+                break;
+            default :
+                throw new Error('정의되지 않은 타입입니다.');
+        }
+        this.memoBox?.prepend(template);
+    }
+
+    private addImage(memo:MemoType):HTMLElement{
+        const {type} = memo;
+        const {title, content} = memo.data;
+        const template = `
+            <div class="memo-image">
+            <img src="${content}" alt="">
+            </div>
+            <div class="memo-title">${title}</div>
+        `;
+        return this.createArticle(template, type);
+    }
+    private addVideo(memo:MemoType):HTMLElement{
+        const {type} = memo;
+        const {title, content} = memo.data;
+        const template = `
+                <div class="memo-video">
+                    <iframe src="${content}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="memo-title">${title}</div>
+        `;
+        return this.createArticle(template, type);
+    }
+    private addText(memo:MemoType):HTMLElement{
+        const {type} = memo;
+        const {title, content} = memo.data;
+        const template = `
+                <p class="memo-title">${title}</p>
+                <p class="memo-desc">${content}</p>
+        `;
+        return this.createArticle(template, type);
+    }
+    private addTask(memo:MemoType):HTMLElement{
+        const {type} = memo;
+        const {title, content} = memo.data;
+        const template = `
+                <p class="memo-title">${title}</p>
+                <ul class="memo-task">
+                    <li>${content}</li>
+                </ul>
+        `;
+        return this.createArticle(template, type);
+    }
+    private createArticle(template:string, type:string):HTMLElement{
+        const article = document.createElement('article');
+        article.setAttribute('data-type', type);
+        article.classList.add('memo');
+        article.innerHTML = template;
+        return article;
+    }
+}
